971710904b4c8beeaa7c4ba7a660edc5d4480906
Son Kwanwoo <sonkw4775@gmail.com>
2024-07-03 16:46:21 +0900

    JavaScript-display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,13 +1,28 @@
 // Fetch the itmes from the JSON file
 function loadItems() {
     return fetch('data/data.json')
-    .then(response => response.json())
-    .then(json => json.items);
+        .then(response => response.json())
+        .then(json => json.items);
 }
 
-// Main
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item_thumbnail"/>
+        <span class="item_description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+// Mains
 loadItems()
     .then(items => {
         console.log(items);
-})
-.catch(console.log);
+        displayItems(items);
+    })
+    .catch(console.log);

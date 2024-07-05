78c01aec4be3cfbfcc654a90914b4f8f7d04ae9c
Son Kwanwoo <sonkw4775@gmail.com>
2024-07-05 13:12:13 +0900

    1-10, 모드 선택 기능 추가

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,36 +1,5 @@
-# def brGame(player):
-#     global num
-#     while True:
-#         try:
-#             n = int(input(f"{player}, 부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : "))
-#             if n in [1, 2, 3]:
-#                 break
-#             else:
-#                 print("1,2,3 중 하나를 입력하세요")
-#         except ValueError:
-#             print("정수를 입력하세요")
-    
-#     for i in range(1, n + 1):
-#         num += 1
-#         print(f"{player} : ", num)
-#         if num == 31:
-#             return True
-#     return False
-
-# num = 0
-
-# while num < 31:
-#     if brGame("playerA"):
-#         print("playerB win!")
-#         break
-    
-#     if num < 31 and brGame("playerB"):
-#         print("playerA win!")
-#         break
-
 import random
 
-#computer mode
 def brGame(player):
     global num
     if player == "computer":
@@ -53,16 +22,42 @@ def brGame(player):
             return True
     return False
 
+def two_player():
+    while num < 31:
+        if brGame("playerA"):
+            print("playerB win!")
+            break
+        
+        if num < 31 and brGame("playerB"):
+            print("playerA win!")
+            break
+        
+def one_player():
+    while num < 31:
+        if brGame("computer"):
+            print("player win!")
+            break
+        
+        if num < 31 and brGame("player"):
+            print("computer win!")
+            break
+
 num = 0
 
-while num < 31:
-    if brGame("computer"):
-        print("player win!")
-        break
-    
-    if num < 31 and brGame("player"):
-        print("computer win!")
-        break
+while True:
+    try:
+        mode = int(input("게임 모드를 선택하세요 (1: 혼자 플레이, 2: 둘이 플레이): "))
+        if mode in [1,2]:
+            break
+        else:
+            print("1 또는 2를 입력하세요")
+    except ValueError:
+        print("1 또는 2를 입력하세요")
+        
+if mode == 1:
+    one_player()
+else:
+    two_player()
 
 # num = 0;
 
